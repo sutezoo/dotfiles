59b2e656d0dac9601c2b2e2e8cffa4414319281b
sutezoo <47258627+sutezoo@users.noreply.github.com>
2023-04-11 23:56:02 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ sudo apt upgrade -y
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
 sudo apt install -y fish
+exec fish
 
 ## fisher
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
