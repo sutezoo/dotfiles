71fadaf284d159f866d0051e3a92ffa632bd661c
yusaku <yusaku@PC-1627.brainpad.co.jp>
2023-04-11 22:56:27 +0900

    install.shを追加

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -ue
+
+sudo apt update
+sudo apt upgrade -y
+
+# fish related installs
+## fish
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install -y fish
+
+## fisher
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install oh-my-fish/theme-bobthefish jethrokuan/fzf decors/fish-ghq jorgebucaran/nvm.fish
+
+## fzf
+sudo apt install -y fzf
+
+# base16-shell
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
+# ghq
+sudo apt install -y build-essential
+## golang(v1.20.3)
+wget https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.20.3.linux-amd64.tar.gz
+/usr/local/go/bin/go install github.com/x-motemen/ghq@latest
+
+git clone https://github.com/x-motemen/ghq
+cd ghq
+make install
+
+# symbolic links
+mkdir ~/backup
+mv ~/.bashrc ~/backup
+mv ~/.tmux.conf ~/backup
+mv ~/.config/fish/config.fish ~/backup
+
+ln -s .bashrc ~/
+ln -s .tmux.conf ~/
+ln -s config.fish ~/.config/fish/
+ln -s config.fish ~/.config/fish/functions/
